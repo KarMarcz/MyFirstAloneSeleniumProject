43e543fff2e5812284ac27c1bc9d3f57adffd8ec
Karol Marczyk <KarMarcz@gmail.com>
2018-12-09 18:34:19 +0100

    Adding Wait Class

---
+++ src/main/java/com/automationpractice/utils/Waits.java
@@ -0,0 +1,19 @@
+package com.automationpractice.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Waits {
+    private static final int EXPLICIT_WAIT_TIMEOUT = 5;
+    private WebDriver driver;
+    public Waits(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void waitForElementToBeClickable(WebElement element){
+        WebDriverWait wait = new WebDriverWait(driver, EXPLICIT_WAIT_TIMEOUT);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+}
