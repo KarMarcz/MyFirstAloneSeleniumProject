9f43d407759a72668cdd8aa4b94d049de836ff9b
Karol Marczyk <KarMarcz@gmail.com>
2018-12-09 17:34:02 +0100

    Adding chrome drivers to Project

--- src/test/java/com/automationpractice/ui/RegistrationTest.java
+++ src/test/java/com/automationpractice/ui/RegistrationTest.java
@@ -1,4 +1,25 @@
 package com.automationpractice.ui;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RegistrationTest {
+    private WebDriver driver;
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chrome/chromedriver_win32.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void registrationTest () {
+        driver.get("http://automationpractice.com/index.php");
+    }
+    @AfterEach
+    public void tearDown () {
+        driver.close();
+    }
 }
